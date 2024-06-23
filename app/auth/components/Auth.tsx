4ece9891c2a8624833de74ddd1ed4aa3b7a3a758
rakotorisonlandry <rakotorisonlandry@gmail.com>
2024-06-23 23:32:00 +0200

    Component Auth updated

--- app/auth/components/Auth.tsx
+++ app/auth/components/Auth.tsx
@@ -1,9 +1,41 @@
 "use client";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 type Variant = "LOGIN" | "REGISTER";
 function Auth() {
   const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIseLoading] = useState(false);
+
+  const toogleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit:SubmitHandler<FieldValues> =(data) =>{
+    setIseLoading(true)
+    if (variant==='REGISTER') {
+        //axios Register
+    }
+    if(variant ==="LOGIN"){
+        //NEXTAuth SignIN
+    }
+  }
   return <div>Auth</div>;
 }
 
