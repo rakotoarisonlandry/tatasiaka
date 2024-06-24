f058b112414c285c7685f261bbe0ff27c8afb84e
rakotorisonlandry <rakotorisonlandry@gmail.com>
2024-06-24 21:23:20 +0200

    simple modif

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 
 function page() {
   return (
-    <div className="flex    items-center justify-center min-h-screen p-12 space-x-40">
-      <div className="w-[80%] flex justify-between rounded-2xl h-[80%] text-black p-2 bg-[white]">
-        <div className="p-11">
+    <div className="flex  items-center justify-center overflow-y-hidden min-h-screen p-10 space-x-32  ">
+      <div className="w-[80%] flex justify-between rounded-2xl h-[70%] text-black p-5 bg-[white]">
+        <div className="p-9 w-[40%]">
           <div className="text-[#BB15CF] mb-5">
             <h2 className="font-semibold text-xl ">
               <span className="text-[30px]">.</span>
@@ -20,9 +20,9 @@ function page() {
           </div>
         </div>
         <Image
-          src="/log.jpg"
+          src=""
           width="500"
-          height="800"
+          height="500"
           className=" rounded-2xl"
           alt="logo"
         />
